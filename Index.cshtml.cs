88573db50f4a0e50f4944e50e18bb78358ca96bb
Sandra Scaffidi <54339234+slscaffidi123abc@users.noreply.github.com>
2019-10-23 14:57:55 -0500

    Navistar Work  in progress

---
+++ Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Navistar.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+
+        }
+    }
+
+    
+}
